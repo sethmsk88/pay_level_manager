4415c700585068a4d39286e7caec6ae42f749673
Seth Kerr <sethmsk88@yahoo.com>
2016-03-09 09:56:23 -0500

    Add Google Analytics Tracking

--- index.php
+++ index.php
@@ -37,6 +37,8 @@
     <![endif]-->
   </head>
   <body>
+    <!-- Google Analytics Tracking -->
+    <?php include_once($_SERVER['DOCUMENT_ROOT'] . "bootstrap\apps\shared\analyticstracking.php") ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
